e4a229aba45f08b1e514c763a5cf7aadb3dea2dd
RossTubes <70896941+RossTubes@users.noreply.github.com>
2022-01-13 10:51:26 +0100

    Goomba waypoints/attack

---
+++ Verticle_Slice_3D/Assets/Goomba/movement.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public int health;
+    private int starthealth = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = starthealth;
+
+    }
+}
