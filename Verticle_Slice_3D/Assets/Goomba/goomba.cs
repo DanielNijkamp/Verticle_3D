c0358e0ea8deb155c2af57a526b6e7a0c547b9f5
RossTubes <70896941+RossTubes@users.noreply.github.com>
2022-01-27 09:49:28 +0100

    Goomba Chase & Walk

--- Verticle_Slice_3D/Assets/Goomba/goomba.cs
+++ Verticle_Slice_3D/Assets/Goomba/goomba.cs
@@ -5,29 +5,70 @@ using UnityEngine.AI;
 
 public class goomba : MonoBehaviour
 {
-    NavMeshAgent agent;
-    public Transform[] waypoints; //array of waypoints
-    int waypointIndex; //index for choosing the poiints
-    Vector3 target;
-    private movement player;
+    public NavMeshAgent agent;
+ 
+    public Transform player;
+
+    public LayerMask whatIsGround, whatIsPlayer;
+
+    private movement _player;
+
+    //Patrol
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    //Goomba Stats
+    public bool isInAttackRange, isInSightRange;
+    public float sightRange, attackRange;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        _player = GameObject.FindGameObjectWithTag("Player").GetComponent<movement>();
         agent = GetComponent<NavMeshAgent>();
-        UpdateDestination();
-        player = GameObject.FindGameObjectWithTag("Player").GetComponent<movement>();
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        if (Vector3.Distance(transform.position, target) < 1) //if our distance to waypoint is less then 1 then increase the waypoint index by one
-        {
-            iterateWaypointIndex();
-            UpdateDestination();
-        }
+        isInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        isInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+
+        if (!isInAttackRange && !isInSightRange) UpdateDestination();
+        if (!isInAttackRange && isInSightRange) ChasePlayer();
+        if (isInAttackRange && isInSightRange) AttackPlayer();
     }
 
+    //Primary Functions
+    void UpdateDestination() 
+    {
+        if (!walkPointSet) SearchWalkPoint();
+
+        if (walkPointSet)
+            agent.SetDestination(walkPoint);
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        //walkpoint reached
+        if (distanceToWalkPoint.magnitude < 1f)
+            walkPointSet = false;
+    }
+
+    public void ChasePlayer()
+    {
+        agent.SetDestination(player.position);
+        transform.LookAt(player.position);
+    }
+
+    public void AttackPlayer()
+    {
+
+    }
+
+    //Secondary Functions
     private void OnTriggerEnter(Collider collider)
     {
         if(collider.tag == "Player")
@@ -37,19 +78,26 @@ public class goomba : MonoBehaviour
     }
     public void Attack()
     {
-        player.health -= 10;
+        _player.health -= 10;
     }
-    void UpdateDestination() //then update the destination by setting the target to current waypoint and the navmesh agent destination to target
+    void SearchWalkPoint()
     {
-        target = waypoints[waypointIndex].position;
-        agent.SetDestination(target);
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
+            walkPointSet = false;
     }
-    void iterateWaypointIndex()
+
+    private void OnDrawGizmosSelected()
     {
-        waypointIndex++;
-        if(waypointIndex== waypoints.Length) //if the waypointindex is egual to the number of waypoints it's get setback to zero
-        {
-            waypointIndex = 0;
-        }
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, sightRange);
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireSphere(transform.position, walkPointRange);
     }
 }
